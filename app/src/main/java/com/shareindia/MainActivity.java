eb2fed9f1936c355315008cae089b9e9c08cce08
Sujeet Suman <sujeetxavier793@gmail.com>
2020-07-21 20:02:25 +0530

    first commit fir the home layout

---
+++ app/src/main/java/com/shareindia/MainActivity.java
@@ -0,0 +1,66 @@
+package com.shareindia;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private DrawerLayout drawer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+         toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // reference of the main layout ie drawer layout
+
+        drawer =findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle =new ActionBarDrawerToggle(this,drawer,toolbar,
+                R.string.open_drawer,R.string.close_drawer);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+    }
+
+//    to close the navigation bar when the task is completed
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START))
+            drawer.closeDrawer(GravityCompat.START);
+        else{
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return true;
+    }
+}
